c1b39ae72ba2231a0506e1ba8649e7852c2d13ce
Brian Cauley <me@briancauley.com>
2026-03-28 13:23:15 -0600

    disable UseHsts()

--- src/Presentation/Connecvita.Web/Program.cs
+++ src/Presentation/Connecvita.Web/Program.cs
@@ -37,7 +37,7 @@ else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    //app.UseHsts();
 }
 
 if (!app.Environment.IsProduction())
